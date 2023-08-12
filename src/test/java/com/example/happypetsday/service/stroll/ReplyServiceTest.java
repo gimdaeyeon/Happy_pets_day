9094188c69e2c1da0df4996f11a1eb36a4687c68
gimdaeyeon <rlaeodus1306@naver.com>
2023-08-12 20:29:14 +0900

    Test : ReplyServiceTest생성

---
+++ src/test/java/com/example/happypetsday/service/stroll/ReplyServiceTest.java
@@ -0,0 +1,37 @@
+package com.example.happypetsday.service.stroll;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReplyServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void findReplyList() {
+    }
+
+    @Test
+    void findTotal() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void removeAll() {
+    }
+
+    @Test
+    void modify() {
+    }
+}

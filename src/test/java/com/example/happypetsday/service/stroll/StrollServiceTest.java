b435d54c324dd9909db3ca5e2259d8aa64adf40b
gimdaeyeon <rlaeodus1306@naver.com>
2023-08-14 21:21:32 +0900

    Test : 산책게시판 테스트파일 생성

---
+++ src/test/java/com/example/happypetsday/service/stroll/StrollServiceTest.java
@@ -0,0 +1,57 @@
+package com.example.happypetsday.service.stroll;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StrollServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void findBoard() {
+    }
+
+    @Test
+    void modifyViewCount() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void getTotal() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void modify() {
+    }
+
+    @Test
+    void findBoardToModify() {
+    }
+
+    @Test
+    void findMainList() {
+    }
+
+    @Test
+    void findBoardSearch() {
+    }
+
+    @Test
+    void findBoardSearchTotal() {
+    }
+}

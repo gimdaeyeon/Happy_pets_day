d672043b2a5ed90414c3f6b3dd4bcd51cc6c51d1
gimdaeyeon <rlaeodus1306@naver.com>
2023-08-15 21:27:55 +0900

    no message

--- src/test/java/com/example/happypetsday/service/stroll/ReplyServiceTest.java
+++ src/test/java/com/example/happypetsday/service/stroll/ReplyServiceTest.java
@@ -1,11 +1,31 @@
 package com.example.happypetsday.service.stroll;
 
+import com.example.happypetsday.dto.StrollReplyDto;
+import com.example.happypetsday.dto.UserDto;
+import com.example.happypetsday.mapper.StrollReplyMapper;
+import com.example.happypetsday.mapper.UserMapper;
+import com.example.happypetsday.service.user.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@ExtendWith(MockitoExtension.class)
+@Slf4j
+@Transactional
 class ReplyServiceTest {
+
+    @Mock
+    private StrollReplyMapper replyMapper;
+
+    @InjectMocks
+    private StrollService strollService;
+    private StrollReplyDto replyDto;
 
     @BeforeEach
     void setUp() {
